0487daeb5e0e9c12cfde3fdc59af88cd94401bca
ManavChaudhary999 <999macboy@gmail.com>
2022-02-23 19:09:42 +0530

    Added redux-persist for session storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from "redux";
+import {persistStore} from "redux-persist";
 
 import logger from "redux-logger"; // library or middelware to debug log our state
 
@@ -6,6 +7,13 @@ import rootReducer from "./root.reducer";
 
 const middlewares = [logger];
 
+// if(process.env.NODE_ENV == 'development')
+// {
+//     middlewares.push(logger);
+// }
+
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor};
